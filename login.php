c444c34ea524e9242a11a213d61d121f09f84424
hasib116 <hasibul.017bd@gmail.com>
2026-09-06 23:45:26 +0600

    login interfase add

---
+++ login.php
@@ -0,0 +1,9 @@
+<form method="post" action="login.php">
+    <label type="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <br>
+    <label type="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <input type="submit" value="Login">
+</form>
