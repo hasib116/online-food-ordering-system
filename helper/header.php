2a91a175a4354fae1b19d1ad9761964007ae5009
hasib116 <hasibul.017bd@gmail.com>
2026-09-06 23:50:31 +0600

    chengaes for condition

--- helper/header.php
+++ helper/header.php
@@ -3,4 +3,8 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
     session_regenerate_id(true);
     setcookie(session_name(), session_id(), time() + 3600, '/');
+    const COOKIE_LIFETIME = 3600; // 1 hour
+    define('COOKIE_LIFETIME', 3600); // 1 hour
+    gettimeofday($now);
+    setcookie(session_name(), session_id(), $now['sec'] + COOKIE_LIFETIME, '/');
 }
